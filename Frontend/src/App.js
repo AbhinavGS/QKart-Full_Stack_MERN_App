39ea8bd995f2d3d852f13ea475c35d03de748aba
Abhinav Sorate <abhinavsorate01@gmail.com>
2023-01-15 01:44:12 +0530

    add backend url

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import Register from "./components/Register";
-import ipConfig from "./ipConfig.json";
+// import ipConfig from "./ipConfig.json";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Products from "./components/Products";
@@ -10,8 +10,8 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 
 export const config = {
-  // endpoint: `https://qkart-frontend-abhinav-sorate.herokuapp.com/api/v1`,
-  endpoint: `http://localhost:8082/api/v1`,
+  endpoint: `https://qkart-backend-afrf.onrender.com/api/v1`,
+  // endpoint: `http://localhost:8082/api/v1`,
 };
 
 function App() {
